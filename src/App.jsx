c8846b95f343f60f1ff8c189cb25b8e9d05fc525
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-10 22:15:35 +0600

    Handle event on click

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  function handleClick() {
+    alert('I am clicked')
+  }
+  
+
+  return (
+    <>
+      <h3>Hello! I am Fatema Tuj Johra Lima</h3>
+      <button onClick={handleClick}>Click me</button>
+
+    </>
+  )
+}
+
+export default App
