52ad175b37592ad8ca0027d7608d7a3eac2e9900
fatematujjohra <fatematujjohralima2@gmail.com>
2026-04-11 23:54:01 +0600

    handle count

--- src/Player.jsx
+++ src/Player.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 export default function Player() {
     const [run,setRun] =useState(0)
+    const [six,setSix] =useState(0)
+    const [four,setFour] =useState(0)
 
     const handleSingle = () => {
         setRun(run+1)
@@ -9,16 +11,19 @@ export default function Player() {
 
     const handleFour = () => {
         setRun(run+4)
+        setFour(four+1)
     }
 
     const handleSix = () => {
         setRun(run+6)
+        setSix(six+1)
     }
 
     return(
         <div>
             <h3>BatsMan</h3>
             <h4>Runs: {run}</h4>
+            <p> fours: {four} | sixes: {six} </p>
             <button onClick={handleSingle}>Single</button>
             <button onClick={handleFour}>Four</button>
             <button onClick={handleSix}>Six</button>
